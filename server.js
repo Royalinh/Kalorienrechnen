b28326e84f99bbbbbfbe96223e5c4a5efb00519c
Hoàng Gia Linh <royalinh@MacBook-Pro-cua-Hoang.local>
2025-06-21 00:04:01 +0200

    fix: use process.env.PORT for Render

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); //gọi thư viện express - framework Tạo web server
 const path = require('path'); //gọi thư viện path để chạy trên mọi hệ điều hành được windows, macos, linux
 const app = express(); //khởi tạo 1 uwsng dụng express
-const port = 3000; //cổng mà server lắng nghe để mở trình duyệt
+const port = process.env.PORT || 3000; //cổng mà server lắng nghe để mở trình duyệt
 
 // Dùng thư mục hiện tại làm nơi phục vụ file tĩnh html , css, js, ảnh
 app.use(express.static(__dirname));
